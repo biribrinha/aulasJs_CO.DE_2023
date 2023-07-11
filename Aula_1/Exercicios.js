5d36db289c360ab8dfa700fe567c9cec6338837f
biribrinha <juliabergman523@gmail.com>
2023-07-11 19:00:26 -0300

    feat: primeiros exercícios

---
+++ Aula_1/Exercicios.js
@@ -0,0 +1,67 @@
+console.log("hello Word!!");
+
+let prompt = require('prompt-sync')()
+
+// let nome = prompt('Digite seu nome: ')
+// console.log('Ola ' + nome)
+
+
+
+
+// let numero = prompt('Digite um número: ')
+// if (numero > 10) {
+//     console.log("Maior que 10")
+// } else if (numero == 10) {
+
+//     console.log("Igual a 10")
+// } else {
+//     console.log("menor que 10")
+// }
+
+
+
+// let letra = prompt("Digite uma letra: ")
+// if (letra == "a") {
+//     console.log("letra a")
+// } else if (letra == "b") {
+//     console.log("letra b")
+// } else if (letra == "c") {
+//     console.log("letra c")
+// } else {
+//     console.log("Letra náo existe")
+// }
+
+// let letra = prompt("Digite uma letra: ")
+// switch (letra) {
+//     case "a":
+//         console.log("letra a")
+//         break;
+
+//     case "b":
+//         console.log("letra b")
+//         break;
+
+//     case "c":
+//         console.log("letra c")
+//         break;
+
+//     case "d":
+//         console.log("letra d")
+//         break;
+
+//     case "e":
+//         console.log("letra e")
+//         break;
+
+//     default:
+//         console.log("Letra náo existe")
+//         break;
+// }
+
+
+const pNUm = prompt("Digite o primeiro numero: ")
+const sNUm = prompt("Digite o segundo numero: ")
+
+const soma = pNUm + sNUm;
+
+console.log("A soma é: " + soma)
