6b54a82a36b2edac95275485f44091991fa77519
biribrinha <juliabergman523@gmail.com>
2023-07-17 21:28:11 -0300

    feat: entrega desafios aula 5

--- Aula_5/Desafio_While.js
+++ Aula_5/Desafio_While.js
@@ -3,12 +3,12 @@ let prompt = require('prompt-sync')()
 // 6. Imprimir números pares de 0 a 20 utilizando o loop while
 
 
-let contador = 0;
+// let contador = 0;
 
-while (contador < 20) {
-    contador = contador + 2;
-    console.log(contador)
-}
+// while (contador < 20) {
+//     contador = contador + 2;
+//     console.log(contador)
+// }
 
 
 
@@ -19,9 +19,40 @@ while (contador < 20) {
 // idade e mostre uma mensagem quando a idade do usuário for
 // maior que 18.
 
+
+// let idade;
+
+// do{
+//     idade = prompt("Sua idade")
+// }while (idade < "18")
+
 // 9. Faça um programa que receba um número do usuário e mostre
 // no console a contagem regressiva até 0.
+
+
+// contador = prompt("Digite um numero: ")
+// while (contador >= 0){
+
+//     console.log(contador);
+//     contador = contador - 1;
+
+// }
 
 // 10. Faça um programa que receba um número do usuário e continue
 // a pedir o número se o numero for diferente de 0.
 
+let soma = 0;
+
+while (soma < 100) {
+
+    soma = soma + 1;
+
+    console.log(soma);
+
+}
+
+// let numero;
+
+// do{
+//     numero = prompt("Digite um numero")
+// }while (numero != "0")
